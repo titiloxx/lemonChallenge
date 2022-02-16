2a80cf1295e0a13409be684c84f98cfec4187ac8
titiloxx <titiloxx@gmail.com>
2022-02-16 11:52:49 -0300

    added message "only /getFoaas supported"

--- src/main/kotlin/com/example/lemon/controller/Api.kt
+++ src/main/kotlin/com/example/lemon/controller/Api.kt
@@ -2,25 +2,23 @@ package com.example.lemon.controller
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.RequestHeader
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("/")
 class Api {
     
-    @RequestMapping("/getFoaas")
+    @RequestMapping("getFoaas")
     fun getFoaas(@RequestHeader("userid") userId:String): ResponseEntity<String> {
         val foaasResponse = ClientService.getFoaasResponse(userId)
         return ResponseEntity(foaasResponse, HttpStatus.OK)
 
     }
+    @RequestMapping("error", method = arrayOf(RequestMethod.GET))
+    fun error(): ResponseEntity<String> = ResponseEntity("Only /getFoaas path is supported", HttpStatus.BAD_REQUEST)
 
-    @ExceptionHandler
+   @ExceptionHandler
     fun exceptionHandler(ex: Exception): ResponseEntity<String> {
         return ResponseEntity(ex.message, HttpStatus.INTERNAL_SERVER_ERROR)
     }
-
 }
